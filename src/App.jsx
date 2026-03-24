025945ef7615110079d9cd5205fe3bc85785eeb5
Yagho Petini <yaghojp@gmail.com>
2026-03-23 21:45:57 -0300

    .

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-10">
+      <h1 className="text-4xl text-red-500">
+        Teste Tailwind 🚀
+      </h1>
+    </div>
+  );
+}
+
+export default App;
